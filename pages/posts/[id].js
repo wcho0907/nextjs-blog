01452dafc5cc29366683b607e93c7439a79f96ba
William Ho <wcho0907@gmail.com>
2023-02-16 15:45:37 +0800

    Test dynamic routing

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,40 +1,10 @@
-import Head from 'next/head';
-import Date from '../../components/date';
-import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import utilStyles from '../../styles/utils.module.css';
+import { useRouter } from "next/router";
 
-export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
-            <title>{postData.title}</title>
-        </Head>
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
-        {postData.title}
-        <br />
-        {postData.id}
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </Layout>
-    );
-}
+  return <p>Post: {id}</p>;
+};
 
-export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-      paths,
-      fallback: false,
-    };
-}
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+export default Post;
